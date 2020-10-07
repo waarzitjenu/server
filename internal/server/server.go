ebfe56905f8cf20a92c51e5aa77d667f9b45a87b
Sultron <firekicks@icloud.com>
2020-10-07 00:35:50 -0700

    Added Gin framework in place of net/http #14

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"go-osmand-tracker/internal/types"
 	"log"
 	"net/http"
@@ -18,16 +19,22 @@ var (
 
 // Listen spins up a webserver and listens for incoming connections
 func Listen(port uint, db *storm.DB) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Server", serverIdentifier)
-		w.Header().Add("Content-Type", "text/plain")
-		w.WriteHeader(501) // Not implemented
-		fmt.Fprintf(w, "Sorry, %s is not implemented.", r.RequestURI)
+
+	var request = gin.Default() // Creates a gin router with default middleware
+
+	request.GET("/", func(c *gin.Context) {
+		c.Header("Server", serverIdentifier)
+		c.Header("Content-Type", "text/plain")
+		c.Writer.WriteHeader(501)
+		c.String(http.StatusNotImplemented, "Sorry, %s is not implemented.", c.Request.RequestURI)
+
+
 	})
 
-	http.HandleFunc("/retrieve", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Server", serverIdentifier)
-		w.Header().Add("Content-Type", "application/json")
+	request.GET("/retrieve", func(c *gin.Context) {
+
+		c.Header("Server", serverIdentifier)
+		c.Header("Content-Type", "application/json")
 
 		var entry []types.Entry
 		db.All(&entry, storm.Limit(1), storm.Reverse())
@@ -36,30 +43,32 @@ func Listen(port uint, db *storm.DB) {
 
 		if len(entry) == 0 {
 			// There is no data. Let's return HTTP Status 204: No Content
-			w.WriteHeader(204) // The server successfully processed the request, but is not returning any content.
+			c.Writer.WriteHeader(204) // The server successfully processed the request, but is not returning any content.
 		} else {
 			// There is data. Let's process it.
 			processedEntry, err := json.Marshal(entry[0])
 			if err != nil {
-				w.WriteHeader(500) // HTTP 500 Internal Server Error
+				c.Writer.WriteHeader(500) // HTTP 500 Internal Server Error
 				log.Println("Error retrieving last database entry", err)
 			} else {
-				w.WriteHeader(200) // HTTP 200 OK
+				c.Writer.WriteHeader(200) // HTTP 200 OK
 				responseData = processedEntry
 			}
 		}
 
-		w.Write(responseData)
+		c.String(http.StatusOK, string(responseData))
+
 	})
 
-	http.HandleFunc("/retrieve/multi", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Server", serverIdentifier)
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(200)
+	request.GET("/retrieve/multi", func(c *gin.Context) {
+		c.Header("Server", serverIdentifier)
+		c.Header("Content-Type", "application/json")
+		c.Writer.WriteHeader(200)
 
 		var cnt uint16 = 10
-		if len(r.URL.Query().Get("count")) > 0 {
-			parsedCountValue, err := strconv.ParseUint(r.URL.Query().Get("count"), 10, 16)
+		c.Request.URL.Query().Get("count")
+		if len(c.Request.URL.Query().Get("count")) > 0 {
+			parsedCountValue, err := strconv.ParseUint(c.Request.URL.Query().Get("count"), 10, 16)
 			if err != nil {
 				log.Println("Error parsing query parameter 'count'", err)
 			} else {
@@ -74,29 +83,34 @@ func Listen(port uint, db *storm.DB) {
 		if err != nil {
 			log.Fatal("Processing entries from database failed", err)
 		}
-		w.Write(responseData)
+
+		c.String(http.StatusOK, string(responseData))
+
+
+
 	})
 
-	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Server", serverIdentifier)
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(204) // The server successfully processed the request, and is not returning any content.
+	request.GET("/submit", func(c *gin.Context) {
+		c.Header("Server", serverIdentifier)
+		c.Header("Content-Type", "application/json")
+		c.Writer.WriteHeader(204) // The server successfully processed the request, and is not returning any content.
+
 
 		// TODO: Error handling
-		retrievedLatitude, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-		retrievedLongitude, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-		retrievedTimestamp, _ := strconv.ParseUint(r.URL.Query().Get("timestamp"), 10, 64)
-		retrievedHdop, _ := strconv.ParseFloat(r.URL.Query().Get("hdop"), 64)
-		retrievedAltitude, _ := strconv.ParseFloat(r.URL.Query().Get("altitude"), 64)
-		retrievedSpeed, _ := strconv.ParseFloat(r.URL.Query().Get("speed"), 64)
+		retrievedLatitude, _ := strconv.ParseFloat(c.Request.URL.Query().Get("lat"), 64)
+		retrievedLongitude, _ := strconv.ParseFloat(c.Request.URL.Query().Get("lon"), 64)
+		retrievedTimestamp, _ := strconv.ParseUint(c.Request.URL.Query().Get("timestamp"), 10, 64)
+		retrievedHdop, _ := strconv.ParseFloat(c.Request.URL.Query().Get("hdop"), 64)
+		retrievedAltitude, _ := strconv.ParseFloat(c.Request.URL.Query().Get("altitude"), 64)
+		retrievedSpeed, _ := strconv.ParseFloat(c.Request.URL.Query().Get("speed"), 64)
 
 		locationUpdate := types.LocationUpdate{
-			retrievedLatitude,
-			retrievedLongitude,
-			retrievedTimestamp,
-			retrievedHdop,
-			retrievedAltitude,
-			retrievedSpeed,
+			Latitude:  retrievedLatitude,
+			Longitude: retrievedLongitude,
+			Timestamp: retrievedTimestamp,
+			Hdop:      retrievedHdop,
+			Altitude:  retrievedAltitude,
+			Speed:     retrievedSpeed,
 		}
 
 		// Checks if the data (in 'locationUpdate') conforms to the types of the struct 'LocationUpdate'
@@ -114,7 +128,11 @@ func Listen(port uint, db *storm.DB) {
 		if err != nil {
 			log.Fatal("Saving entry to database failed ", err)
 		}
+
+
 	})
+
+
 
 	var listenAddr string = fmt.Sprintf(":%d", port)
 	fmt.Printf("Starting server at port: %v\n", port)
