c8f0ac764aa2534d1cbfb4a34734404298616df3
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-10-15 19:01:19 +0200

    Added support for the web-app to the Go application

--- internal/auxillary/auxiliary.go
+++ internal/auxillary/auxiliary.go
@@ -4,6 +4,15 @@ import (
 	"os"
 )
 
+// DoesDirExist checks if a directory exists and returns a boolean.
+func DoesDirExist(directory string) bool {
+	_, err := os.Stat(directory)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
 // CreateDirIfNotExist first checks if a directory exists and creates it if does not exist
 func CreateDirIfNotExist(dir string) error {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
