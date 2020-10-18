042f4f83134697c217018f15ed67c29befdf2339
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-10-18 20:15:57 +0200

    Removed the user name and group
    ... not necessary.

--- web/docker.sh
+++ web/docker.sh
@@ -2,7 +2,7 @@
 if [ -d ./dist ]; then rm -r ./dist; fi
 mkdir -p ./dist
 docker build -t ricardobalk/go-osmand-tracker .
-docker run --rm --user "$(id -u):$(id -g)" \
+docker run --rm \
   --mount type=bind,source="$(pwd)"/src/,target=/home/node/go-osmand-tracker/src/,readonly \
   --mount type=bind,source="$(pwd)"/public/,target=/home/node/go-osmand-tracker/public/,readonly \
   --mount type=bind,source="$(pwd)"/.env.local,target=/home/node/go-osmand-tracker/.env.local,readonly \
